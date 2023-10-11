eb75e9c7bc0e351fc24179f756f8894614a05e19
G00410024@atu.ie <G00410024@atu.ie>
2023-10-11 14:30:12 +0100

    Project Setup

---
+++ src/main/java/ie/atu/confirm_and_register/ConfirmAndRegisterApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.confirm_and_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfirmAndRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfirmAndRegisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/confirm_and_register/ConfirmAndRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.confirm_and_register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfirmAndRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
