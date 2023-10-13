6529c73ea1e7d4db9d1fbda4c7904c5d4cc5da51
G00410024@atu.ie <G00410024@atu.ie>
2023-10-13 17:55:07 +0100

    Added RegistrationServiceClient to create interface of example object to handle requests to another port (new microservice). Updated UserController to receive incoming data from new microservice to output to terminal once data pass-through on new microservice is complete.

---
+++ src/main/java/ie/atu/confirm_and_register/RegistrationServiceClient.java
@@ -0,0 +1,11 @@
+package ie.atu.confirm_and_register;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "confirmation-service", url = "http://localhost:8081")
+public interface RegistrationServiceClient {
+   @PostMapping("/confirm")
+    String inputtedDetails(@RequestBody UserDetails userDetails);
+}
